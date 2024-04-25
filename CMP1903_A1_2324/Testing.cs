a85f48c1ed52e3c3d356bd5e4f759707ae7cc9ae
naruto08060119487 <160050698+naruto08060119487@users.noreply.github.com>
2024-04-25 23:03:44 +0100

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -13,15 +13,46 @@ namespace CMP1903_A1_2324
          * This class should test the Game and the Die class.
          * Create a Game object, call the methods and compare their output to expected output.
          */
+        /// <summary>
+        /// creates an instance of the sevens out and three or more class game, and uses debug.assert to test the game and the stats class as an argument
+        /// </summary>
         public void GameTest() //GameTest method
         {
-            //CREATES A GAME OBJECT 
             Game game = new Game();
-            Console.WriteLine("Test 1");
-            int test1 = game.PLay(); //this uses the game class method
+            Statistics stats = new Statistics(); //instance of the statistics class
+            SevensOut sevensOut = new SevensOut(); //instance of the sevensout
+            ThreeOrMore threeOrMore = new ThreeOrMore(); //instance of the three or more
+
+            //placed the class in a while loop to handle input errors
+            bool test = true;
+            while (test)
+            {
+                Console.WriteLine("1. test three or more");
+                Console.WriteLine("2. test sevens out");
+                Console.WriteLine("3. Test Game");
+                string comp = Console.ReadLine();
+                switch (comp) //used switch instead of ifs and else ifs
+                {
+                    case "1" :
+                        Debug.Assert(threeOrMore.ThreeOrMoreGame(stats) >=20, "THREE OR MORE CLASS OK"); // tests the three or more
+                        break;
+                    case "2":
+                        Debug.Assert(sevensOut.SevensOutClass(stats) > 7, "sevens out class passed"); //tests the sevens out class
+                        break;
+                    case "3":
+                        Debug.Assert(game.PLay() == game.PLay(), "game test failed"); //checks the game
+                        break;
+                    default:
+                        Console.WriteLine("invalid entry pick between 1 or 2");
+                        continue;
+                }
+
+                test = false;
+
+            }
+            //CREATES A GAME OBJECT 
 
 
-            Debug.Assert(test1 < 3 || test1 > 18,"Game test Failed"); //this compares the outcome of the game to see is the total is
             //within 3 - 18
 
             /* Create a Die object and call its method.
